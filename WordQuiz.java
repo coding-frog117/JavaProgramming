106882a3fa47f6be48e773aa4d2ee05b84d9978f
송예은 <110962765+coding-frog117@users.noreply.github.com>
2023-05-08 14:00:02 +0900

    Update WordQuiz.java

--- WordQuiz.java
+++ WordQuiz.java
@@ -43,34 +43,29 @@ public class WordQuiz {
         v.add(new Word("cat","고양이"));
         v.add(new Word("map","지도"));
 
-
         System.out.println("****영어 단어 테스트 프로그램 \"명품영어\"입니다.****");
 
         while (true) {
             System.out.println("단어 테스트: 1, 단어 삽입: 2, 종료:3>>");
             int n = scanner.nextInt();
             if (n == 1) {
-                int randomCount = (int)(Math.random()*v.size());
-                Word selectedWord = v.get(randomCount);
-
                 System.out.println("현재"+v.size()+"개의 단어가 들어있습니다. -1을 입력하면 테스트를 종료합니다.");
-                int n2 = scanner.nextInt();
-                if (n2 == -1) {
-                    continue;
-                }
-                else {
+                
+                while (true) {
                     Vector<String> choices = new Vector<String>();
+                    
+                    int randomCount = (int)(Math.random()*v.size());
+                    Word selectedWord = v.get(randomCount);
                     choices.add(selectedWord.y);
+                    
                     while (choices.size() < 4) {
-                        int i = (int)(Math.random()*v.size());
-                        String choice = v.get(i).y;
-                        if (!choices.contains(choice)) {
-                            choices.add(choice);
+                    int i = (int)(Math.random()*v.size());
+                    String choice = v.get(i).y;
+                    if (!choices.contains(choice)) {
+                        choices.add(choice);
                         }
                     } 
-
-                    System.out.println(choices);
-
+                       
                     Collections.shuffle(choices); // 보기 순서 섞기
 
                     // 문제와 보기 출력
@@ -78,9 +73,46 @@ public class WordQuiz {
                     for(int i1 = 0; i1 < 4; i1++) {
                         System.out.println((i1+1) + ". " + choices.get(i1));
                     }
+                        
+                    int chooseNum = scanner.nextInt();
+                        
+                    if (chooseNum == -1) {
+                    	break;
+                    }
                     
-                } // 중괄호 추가
+                    if (choices.get(chooseNum-1).equals(selectedWord.y)) {
+                        System.out.println("Excellent !!");
+                    }
+                    else {
+                    	System.out.println("No !!");
+                    }
+       
+                }
             }
+                
+            else if (n == 2) {
+                	//단어 삽입
+            	System.out.println("영어단어에 그만을 입력하면 입력을 종료합니다");
+            	
+            	while(true) {         	
+            	System.out.println("영어 한글 입력 >>");
+            	String englishWord = scanner.next();
+            	
+            	if (englishWord.equals("그만")){
+            		break;
+            	}
+            	
+            	String koreanWord = scanner.next();
+            	v.add(new Word(englishWord,koreanWord));     	     	
+            	}
+            }
+            
+            else if (n == 3) {
+                System.out.println("\"명품영어\"를 종료합니다");
+                break;
+            }
+            
         }
     }
+      
 }
